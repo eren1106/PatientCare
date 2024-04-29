bd9473b88883cf1b64af41038621f2ec684ef5d1
eren <erenkuek1106@gmail.com>
2024-04-29 23:08:17 +0800

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
-import { Button } from './components/ui/button'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import DoctorDashboardPage from './pages/DoctorDashboardPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/doctor",
+    element: <DoctorDashboardPage />,
+  },
+]);
+
 
 function App() {
 
   return (
     <>
-      <Button>Test shad</Button>
+      <RouterProvider router={router} />
     </>
   )
 }
