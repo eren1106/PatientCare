0ea93c4d6eaa75b0739029ada5ea3c503df470bb
eren <erenkuek1106@gmail.com>
2024-05-17 14:27:15 +0800

    setup patient

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,26 @@
-import { BarChart2, Calendar, Users } from "lucide-react"
+import { BarChart2, Calendar, Dumbbell, Home, Users } from "lucide-react"
 import { Link, useLocation } from "react-router-dom"
 import { Button } from "./ui/button"
 import { cn } from "@/lib/utils"
 import { DASHBOARD_ROOT_PATH } from "@/constants"
+
+const PATIENT_NAV_ITEMS = [
+  {
+    to: "",
+    title: "Home",
+    icon: <Home />
+  },
+  {
+    to: "exercises",
+    title: "Exercises",
+    icon: <Dumbbell />
+  },
+  {
+    to: "appointments",
+    title: "Appointments",
+    icon: <Calendar />
+  }
+]
 
 const DASHBOARD_NAV_ITEMS = [
   {
@@ -22,17 +40,27 @@ const DASHBOARD_NAV_ITEMS = [
   }
 ]
 
-const Sidebar = () => {
+interface SidebarProps {
+  isDoctor?: boolean;
+}
+
+const Sidebar = ({ isDoctor=false }: SidebarProps) => {
   const location = useLocation();
+  const navItems = isDoctor ? DASHBOARD_NAV_ITEMS : PATIENT_NAV_ITEMS;
+  const checkIsSelected = (to: string): boolean => {
+    if (isDoctor) {
+      const isDashboardRoot = location.pathname === `/${DASHBOARD_ROOT_PATH}`;
+      return isDashboardRoot && to === "" || location.pathname === `/${DASHBOARD_ROOT_PATH}/${to}`;
+    }
+    return location.pathname === `/${to}` || (location.pathname === to && to==="")
+  }
 
   return (
     <div className='h-full w-60 flex flex-col items-center p-6 gap-3 border-r'>
       {
-        DASHBOARD_NAV_ITEMS.map((item) => {
+        navItems.map((item) => {
           // to make selected tab have color
-          const isDashboardRoot = location.pathname === `/${DASHBOARD_ROOT_PATH}`;
-          const isActive = isDashboardRoot && item.to === "" || location.pathname === `/${DASHBOARD_ROOT_PATH}/${item.to}`;
-          const selectedClassname = isActive ? "bg-secondary" : "";
+          const selectedClassname = checkIsSelected(item.to) ? "bg-secondary" : "";
 
           return (
             <Link to={item.to} className="w-full">
---
+++ src/pages/patient/ExercisePage.tsx
@@ -0,0 +1,7 @@
+const ExercisePage = () => {
+  return (
+    <div>ExercisePage</div>
+  )
+}
+
+export default ExercisePage
