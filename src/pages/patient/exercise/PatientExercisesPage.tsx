9c276ee4a7032cf9a62a8bacd4a2cc1b8873b062
eren <erenkuek1106@gmail.com>
2024-12-06 20:24:08 +0800

    fix exercise specifications not showing

--- src/pages/patient/exercise/PatientExercisesPage.tsx
+++ src/pages/patient/exercise/PatientExercisesPage.tsx
@@ -22,7 +22,7 @@ const PatientExercisesPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-4 max-w-[40rem] w-full mx-auto">
+    <div className="flex flex-col gap-4 max-w-[50rem] w-full mx-auto">
       <h2>Exercises that are assigned to you</h2>
       {
         isLoading ? <SkeletonCard /> : (
